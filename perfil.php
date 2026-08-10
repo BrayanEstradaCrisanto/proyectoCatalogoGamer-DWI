f1a6d4207eb86bbf4a8f0b7cdca4323b10b1932e
BrayanEstradaCrisanto <dsm23190014.bestrada@alumnos.utsv.edu.mx>
2026-08-10 17:51:51 -0600

    modificacion para cambiar ruta del css en perfil.php

--- perfil.php
+++ perfil.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['usuario'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil de Usuario</title>
-    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="css/perfil.css">
 </head>
 <body>
     <div class="login-container" style="width: 380px;">
